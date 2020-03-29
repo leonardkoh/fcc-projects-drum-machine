fd4bd6b97125d5c4a91e13ab6e5ecde57266c55d
leonard <40259905+leonardkoh@users.noreply.github.com>
2020-03-30 01:02:25 +1100

    6 of 7 passed

--- src/App.js
+++ src/App.js
@@ -1,52 +1,29 @@
 import React from 'react';
 import './App.css';
 
-class DrumPadButton extends React.Component {
-  constructor(props) {
-    super(props)
-    
-    this.handleclick = this.handleclick.bind(this);
-  }
-  
-  handleclick() {
-    return new Audio(this.props.sound).play();
-  }
-
-  render() {
-    return(
-      <button id={this.props.dpb} className="drum-pad btn btn-outline-dark px-4 py-3" onClick={this.handleclick}> {this.props.dpb}
-        <audio id={this.props.dpb} className="clip" src={this.props.sound}>
-        </audio>  
-      </button>
-    );
-  }
-}
+const KEYDATA = {
+  'Q': [81,'Heater-1','https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'], 
+  'W': [87,'Heater-2','https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'],
+  'E': [69,'Heater-3','https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'],
+  'A': [65,'Heater-4','https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'],
+  'S': [83,'Clap','https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'],
+  'D': [68,'Open-HH','https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'],
+  'Z': [90,"Kick-n'-Hat",'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'],
+  'X': [88,'Kick','https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'],
+  'C': [67,'Closed-HH','https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3']
+};
 
 class DrumPad extends React.Component {
   constructor(props) {
     super(props)
   }
-  
-  render() {
 
+  render() {
     return(
-      <div className='container'>
-        <div className='row'>
-          <DrumPadButton dpb='Q' sound='./sounds/Q.mp3'/>
-          <DrumPadButton dpb='W' sound='./sounds/W.mp3'/>
-          <DrumPadButton dpb='E' sound='./sounds/E.mp3'/>
-        </div>
-        <div className='row'>
-          <DrumPadButton dpb='A' sound='./sounds/A.mp3'/>
-          <DrumPadButton dpb='S' sound='./sounds/S.mp3'/>
-          <DrumPadButton dpb='D' sound='./sounds/D.mp3'/>
-        </div>
-        <div className='row'>
-          <DrumPadButton dpb='Z' sound='./sounds/Z.mp3'/>
-          <DrumPadButton dpb='X' sound='./sounds/X.mp3'/>
-          <DrumPadButton dpb='C' sound='./sounds/C.mp3'/>          
-        </div>
-      </div>
+      <button id={this.props.soundUri} className="drum-pad btn btn-outline-dark px-4 py-3" onClick={this.props.clickPlay} value={this.props.msg}> 
+      {this.props.keyID}
+        <audio id={this.props.keyID} className="clip" src={this.props.soundUri} type="audio/mp3"></audio>  
+      </button>
     );
   }
 }
@@ -55,15 +32,72 @@ class App extends React.Component {
   constructor(props) {
     super(props) 
 
+    this.state = {
+      keyCode: '',
+      msg: 'No Sound',
+      currentSound: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.playSound = this.playSound.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleClick(e) {
+    this.setState({
+      currentSound: e.target.children[0].id,
+      msg: e.target.value
+    });
+  }
+
+  handleKeyPress(e) {
+    // if(Object.values(KEYDATA).indexOf(e.keyCode !== -1))
+    //   console.log("YES");
+    // console.log(e.target.value)
+    // let obj = Object.values(KEYDATA).filter(v => v[0] === e.keyCode);
+    // if(Object.values(obj[0]).includes[e.keyCode])
+      // console.log('YES');
+    // this.setState({
+    //   keyCode: obj[0][0],
+    //   msg: obj[0][1],
+    //   currentSound: obj[0][2]
+    // })
+  }
+
+  playSound(){
+    document.getElementById(this.state.currentSound).play();
   }
 
   render() {
+    let message = this.state.msg;
+    if(this.state.currentSound)
+      this.playSound();
+
     return (
       <container id="drum-machine" className="row py-4" >
-        <container id="display" className="row py-4">
+        <container className="row py-4 mt-5">
           <div className="col-2"></div>
-          <div className="col-8">
-            <DrumPad />
+          <div className="col-8 text-center">
+            <div className='container'>
+            <h1>Drum Machine</h1>
+            <br />
+              { Object.entries(KEYDATA).map(([k,v]) => {
+                return (<DrumPad 
+                  keyID={k} 
+                  keyCode={v[0]} 
+                  msg={v[1]} 
+                  soundUri={v[2]}
+                  clickPlay={this.handleClick}
+                />)
+              })
+              }
+            </div>
+            <br />
+            <p id="display" class="py-3">Sound: {message}</p>
           </div>
           <div className="col-2"></div>
         </container>
