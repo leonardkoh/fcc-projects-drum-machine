147acb76e771036c42410af72441901204c98d6f
adminlk <adminlk@adminlk.local>
2020-03-29 17:10:01 +1100

    styling changes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,66 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DrumPadButton extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return(
+      <div class="drum-pad">
+        <button class="btn btn-outline-dark">BUTTON</button>
+      </div>
+    );
+  }
+}
+
+class DrumPad extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  render() {
+    return(
+      <div class='container'>
+        <div class='row'>
+          <DrumPadButton />
+          <DrumPadButton />
+          <DrumPadButton />
+        </div>
+        <div class='row'>
+          <DrumPadButton />
+          <DrumPadButton />
+          <DrumPadButton />
+        </div>
+        <div class='row'>
+          <DrumPadButton />
+          <DrumPadButton />
+          <DrumPadButton />
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props) 
+  }
+
+  render() {
+    return (
+      <container id="drum-machine" class="row py-4" >
+        <container class="row py-4" id="display">
+          <div class="col-2"></div>
+          <div class="col-8">
+            <DrumPad />
+          </div>
+          <div class="col-2"></div>
+        </container>
+      </container>
+      );
+  }
 }
 
 export default App;
